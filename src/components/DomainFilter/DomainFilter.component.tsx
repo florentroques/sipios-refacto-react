92fa4b3f2136d5fab75d85c880b281622034c191
Florent Roques <florentroques@users.noreply.github.com>
2024-03-14 14:49:22 +0100

    MOVE renderSelects logic to renderSelects functino

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -10,6 +10,34 @@ interface Props {
   domains: string[]
 }
 
+function renderSelects(state: State) {
+  const {countries, classifications, subClassifications} = state || {
+    countries: [],
+    classifications: [],
+    subClassifications: []
+  };
+
+  return (
+    <>
+      <select name="countries" multiple>
+        {countries.map(country => (
+          <option value={country} key={country}>{country}</option>
+        ))}
+      </select>
+      <select name="classifications" multiple>
+        {classifications.map(classification => (
+          <option value={classification} key={classification}>{classification}</option>
+        ))}
+      </select>
+      <select name="subClassifications" multiple>
+        {subClassifications.map(subClassification => (
+          <option value={subClassification} key={subClassification}>{subClassification}</option>
+        ))}
+      </select>
+    </>
+  );
+}
+
 class DomainFilter extends React.Component<Props, State> {
   componentDidMount() {
     const { domains } = this.props
@@ -57,29 +85,7 @@ class DomainFilter extends React.Component<Props, State> {
   }
 
   render() {
-    const {countries, classifications, subClassifications} = this.state || {
-      countries: [],
-      classifications: [],
-      subClassifications: []
-    };
-
-    return (<>
-      <select name="countries" multiple>
-        {countries.map(country => (
-          <option value={country} key={country}>{country}</option>
-        ))}
-      </select>
-      <select name="classifications" multiple>
-        {classifications.map(classification => (
-          <option value={classification} key={classification}>{classification}</option>
-        ))}
-      </select>
-      <select name="subClassifications" multiple>
-        {subClassifications.map(subClassification => (
-          <option value={subClassification} key={subClassification}>{subClassification}</option>
-        ))}
-      </select>
-    </>)
+    return renderSelects(this.state);
   }
 }
 
