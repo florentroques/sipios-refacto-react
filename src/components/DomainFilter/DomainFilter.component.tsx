d53b21a8f9fd433f3b4568fec5f088e80ca12645
Florent Roques <florentroques@users.noreply.github.com>
2024-03-14 14:47:39 +0100

    FIX state management in componentDidMount

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -13,33 +13,46 @@ interface Props {
 class DomainFilter extends React.Component<Props, State> {
   componentDidMount() {
     const { domains } = this.props
-    this.setState({
+
+    let state = this.state || ({
       countries: [],
       classifications: [],
       subClassifications: []
     });
 
     for(let i = 0; i < domains.length; i++) {
-      if (this.state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
-        this.state.countries.push(domains[i].substring(0,2))
+      if (state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
+        state = ({
+          ...state,
+          countries: [...state.countries, domains[i].substring(0,2)]
+        })
       }
-      this.state.classifications.push(domains[i].substring(3,5));
+
+      state = ({
+        ...state,
+        classifications: [...state.classifications, domains[i].substring(3,5)]
+      })
+
       let flag = false;
-      for(let j = 0; j < this.state.subClassifications.length; j++) {
-        if (this.state.subClassifications[j] === domains[i].substring(6)) {
+      for(let j = 0; j < state.subClassifications.length; j++) {
+        if (state.subClassifications[j] === domains[i].substring(6)) {
           flag = true
           break;
         }
       }
       if (!flag) {
-        this.state.subClassifications.push(domains[i].substring(6));
+        state = ({
+          ...state,
+          subClassifications: [...state.subClassifications, domains[i].substring(6)]
+        })
       }
     }
 
     this.setState({
-      ...this.state,
-      classifications: this.state.classifications.filter((e, i, l) => l.indexOf(e) === i),
+      ...state,
+      classifications: state.classifications.filter((e, i, l) => l.indexOf(e) === i),
     })
+
     this.forceUpdate()
   }
 
