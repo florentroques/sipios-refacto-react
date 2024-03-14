c7fa51c4af3fae9838ff174dc259024ccb1987a0
Florent Roques <florentroques@users.noreply.github.com>
2024-03-14 15:02:01 +0100

    REFACTOR initialize state to initializeState() function

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -38,48 +38,50 @@ function renderSelects(state: State) {
   );
 }
 
-class DomainFilter extends React.Component<Props, State> {
-  componentDidMount() {
-    const { domains } = this.props
+function initializeState(domains: string[]) {
+  let state: State = {
+    countries: [],
+    classifications: [],
+    subClassifications: []
+  };
 
-    let state = this.state || ({
-      countries: [],
-      classifications: [],
-      subClassifications: []
-    });
-
-    for(let i = 0; i < domains.length; i++) {
-      if (state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
-        state = ({
-          ...state,
-          countries: [...state.countries, domains[i].substring(0,2)]
-        })
-      }
-
+  for(let i = 0; i < domains.length; i++) {
+    if (state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
       state = ({
         ...state,
-        classifications: [...state.classifications, domains[i].substring(3,5)]
+        countries: [...state.countries, domains[i].substring(0,2)]
       })
-
-      let flag = false;
-      for(let j = 0; j < state.subClassifications.length; j++) {
-        if (state.subClassifications[j] === domains[i].substring(6)) {
-          flag = true
-          break;
-        }
-      }
-      if (!flag) {
-        state = ({
-          ...state,
-          subClassifications: [...state.subClassifications, domains[i].substring(6)]
-        })
-      }
     }
 
-    this.setState({
+    state = ({
       ...state,
-      classifications: state.classifications.filter((e, i, l) => l.indexOf(e) === i),
+      classifications: [...state.classifications, domains[i].substring(3,5)]
     })
+
+    let flag = false;
+    for(let j = 0; j < state.subClassifications.length; j++) {
+      if (state.subClassifications[j] === domains[i].substring(6)) {
+        flag = true
+        break;
+      }
+    }
+    if (!flag) {
+      state = ({
+        ...state,
+        subClassifications: [...state.subClassifications, domains[i].substring(6)]
+      })
+    }
+  }
+
+  return {
+    ...state,
+    classifications: state.classifications.filter((e, i, l) => l.indexOf(e) === i),
+  };
+}
+
+class DomainFilter extends React.Component<Props, State> {
+  componentDidMount() {
+    this.setState(initializeState(this.props.domains))
   }
 
   render() {
