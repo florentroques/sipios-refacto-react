43f7c35d4a3f7bb38b97160153154cf994403a52
Florent Roques <florentroques@users.noreply.github.com>
2024-03-14 19:44:46 +0100

    ADD extract codes from domain

--- src/components/common/initializeState.ts
+++ src/components/common/initializeState.ts
@@ -1,5 +1,17 @@
 import { State } from './types';
 
+function extractCountryCode(domain: string) {
+  return domain.substring(0,2);
+}
+
+function extractClassificationCode(domain: string) {
+  return domain.substring(3,5);
+}
+
+function extractSubClassificationCode(domain: string) {
+  return domain.substring(6)
+}
+
 export function initializeState(domains: string[]) {
   let state: State = {
     countries: [],
@@ -7,30 +19,36 @@ export function initializeState(domains: string[]) {
     subClassifications: []
   };
 
-  for(let i = 0; i < domains.length; i++) {
-    if (state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
+  for (let i = 0; i < domains.length; i++) {
+    const countryCode = extractCountryCode(domains[i]);
+    const classificationCode = extractClassificationCode(domains[i]);
+    const subClassificationCode = extractSubClassificationCode(domains[i]);
+
+    if (state.countries.indexOf(countryCode) <= 0) {
       state = ({
         ...state,
-        countries: [...state.countries, domains[i].substring(0,2)]
+        countries: [...state.countries, countryCode]
       })
     }
 
     state = ({
       ...state,
-      classifications: [...state.classifications, domains[i].substring(3,5)]
+      classifications: [...state.classifications, classificationCode]
     })
 
     let flag = false;
-    for(let j = 0; j < state.subClassifications.length; j++) {
-      if (state.subClassifications[j] === domains[i].substring(6)) {
-        flag = true
+
+    for (let j = 0; j < state.subClassifications.length; j++) {
+      if (state.subClassifications[j] === subClassificationCode) {
+        flag = true;
         break;
       }
     }
+
     if (!flag) {
       state = ({
         ...state,
-        subClassifications: [...state.subClassifications, domains[i].substring(6)]
+        subClassifications: [...state.subClassifications, subClassificationCode]
       })
     }
   }
